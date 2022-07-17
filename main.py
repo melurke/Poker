81c9985d67d88eb44e55d25e9a6eba3b717e1f59
melurke <standartbrostv@gmail.com>
2022-07-17 14:38:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,39 +7,36 @@ table = []
 
 print("Hand:")
 for i in range(2):
-    hand.append((input("What is the symbol of the card? "), int(input("What is the value of the card? "))))
+	hand.append((input("What is the symbol of the card? "), int(input("What is the value of the card? "))))
 
 for c in combinations.copy():
-    if (hand[0] in c or hand[1] in c) and not (hand[0] in c and hand[1] in c):
-        combinations.remove(c)
+	if (hand[0] in c or hand[1] in c) and not (hand[0] in c and hand[1] in c):
+		combinations.remove(c)
 
 try:
-    probability = (combinations.index((value(hand[0], hand[1]), hand[0], hand[1]))+1)/len(combinations)
+	probability = (combinations.index((value(hand[0], hand[1]), hand[0], hand[1]))+1)/len(combinations)
 except ValueError:
-    probability = (combinations.index((value(hand[0], hand[1]), hand[1], hand[0]))+1)/len(combinations)
+	probability = (combinations.index((value(hand[0], hand[1]), hand[1], hand[0]))+1)/len(combinations)
 if probability < 0.1:
-    probability = ("0" +str(probability*100))[0:5] + "%"
+	probability = ("0" +str(probability*100))[0:5] + "%"
 else:
-    probability = str(probability*100)[0:5] + "%"
+	probability = str(probability*100)[0:5] + "%"
 
 print(probability)
 
-
-""" ------ Under construction --------
 print("Table:")
 for i in range(3):
-    table.append((input("What is the symbol of the card? "), int(input("What is the value of the card? "))))
+	table.append((input("What is the symbol of the card? "), int(input("What is the value of the card? "))))
 
 for c in combinations.copy():
-    if (table[0] in c or table[1] in c or table[2] in c) and not (table[0] in c and table[1] in c) and not (table[1] in c and table[2] in c) and not (table[0] in c and table[2] in c):
-        combinations.remove(c)
+	if (table[0] in c or table[1] in c or table[2] in c) and not (table[0] in c and table[1] in c) and not (table[1] in c and table[2] in c) and not (table[0] in c and table[2] in c):
+		combinations.remove(c)
 
 table_values = []
 
 for c in combinations:
-    cards = table
-    cards.append(c[1])
-    cards.append(c[2])
-    table_values.append(value_5(cards))
-    print(table_values[-1])
-"""
+	cards = table
+	cards.append(c[1])
+	cards.append(c[2])
+	table_values.append(value_5(cards))
+	print(table_values[-1])
